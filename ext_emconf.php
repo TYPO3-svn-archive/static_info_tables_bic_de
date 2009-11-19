c2972297c2f60688eded006ad64ecb2e64b8faec
mstaatz <mstaatz@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-19 13:55:53 +0000

    Initial starting with this branch
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/static_info_tables_bic_de/branches/0.5.x@26718 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "static_info_tables_bic_de"
+#
+# Auto generated 19-11-2009 09:14
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static info tables - German Bank Identifier Codes',
+	'description' => 'Database with german bank identifier codes and the associated bank names.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.5.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Michael Staatz',
+	'author_email' => 'info@mduttlinger.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.0.0-0.0.0',
+			'php' => '5.0.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:20:"class.ext_update.php";s:4:"5c9a";s:12:"ext_icon.gif";s:4:"90d1";s:14:"ext_tables.php";s:4:"26c6";s:25:"ext_tables_static+adt.sql";s:4:"3376";s:19:"icon_static_bic.gif";s:4:"90d1";s:16:"locallang_db.php";s:4:"4dda";s:7:"tca.php";s:4:"99d4";s:10:"doc/README";s:4:"e229";}',
+);
+
+?>
